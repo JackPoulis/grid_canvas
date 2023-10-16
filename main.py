db065d00bd4b9465d4e5ea1cf14d2356a99d2971
Iakovos Poulis <jackpoulis@gmail.com>
2023-10-16 20:16:52 +0300

    Add while loop for pygame window

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+from sys import exit
 
-width = 1920
-height = 1080
+game_name = "LED grid"
+width = 1000
+height = 1000
 
 pygame.init()
+pygame.display.set_caption(game_name)
 screen = pygame.display.set_mode((width,height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    
+    pygame.display.update()
