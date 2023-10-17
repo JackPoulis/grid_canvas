258562f76c41921411744a2890f42743b494d70c
JackPoulis <jackpoulis@gmail.com>
2023-10-17 13:54:54 +0300

    Generate test grid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import random
 from sys import exit
 
 class Grid():
@@ -13,6 +14,10 @@ def cell_position(c,r,buffer):
     x = c*buffer
     y = r*buffer
     return (x,y)
+
+def random_color():
+    levels = range(32,256,32)
+    return tuple(random.choice(levels) for _ in range(3))
 
 game_name = "LED grid"
 
@@ -32,6 +37,12 @@ pygame.display.set_caption(game_name)
 # screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
+
+for r in range(rows):
+        for c in range(columns):
+            pos = cell_position(c,r,cell_size)
+            rect = pygame.Rect(pos[0], pos[1], cell_size, cell_size)
+            pygame.draw.rect(screen,random_color(),rect)
 
 while True:
     for event in pygame.event.get():
