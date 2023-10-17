51a0a7858e83640e21b86fdd96bd0bdc704c93f1
Iakovos Poulis <jackpoulis@gmail.com>
2023-10-17 10:50:20 +0300

    Add fullscreen and esc key quit functionality

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ height = 1000
 
 pygame.init()
 pygame.display.set_caption(game_name)
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             pygame.quit()
             exit()
 
