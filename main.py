23d7e6c8ac4a841bc67af256af2634226a2d5cca
JackPoulis <jackpoulis@gmail.com>
2023-10-20 17:36:18 +0300

    Move game logic from runtime loop to classes

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import pygame
 import numpy as np
-import random
 from sys import exit
-
-class Game():
-    def __init__(self, name, map: np.array):
-        self.name = name
-        self.map = map
-        self.x = map.shape[0]
-        self.y = map.shape[1]
 
 class Display():
     def __init__(self, columns, rows, background=(0,0,0), position = (0,0), map_size = (32, 32), wall = 3):
@@ -35,6 +27,51 @@ class Display():
             self.r = player_y - self.wall
             if self.r < 0: self.r = 0
 
+class Player():
+    def __init__(self, position = (0,0), color = (0,0,255)):
+        self.c = position[0]
+        self.r = position[1]
+        self.color = color
+
+class Mode():
+    def __init__(self, name, map: np.array, display, logic, players = None):
+        self.name = name
+        self.map = map
+        self.cols = map.shape[0]
+        self.rows = map.shape[1]
+        self.p = players
+        self.logic = logic
+        self.display = display
+        self.frame = np.full((display.columns, display.rows, 3), (0,0,0))
+
+def maze_logic(mode, input):
+    point = input[0]
+    player_dc = mode.p[0].c - mode.display.c
+    player_dr = mode.p[0].r - mode.display.r
+    p_c = mode.p[0].c
+    p_r = mode.p[0].r
+    if point:
+        if point[1] == player_dr:
+            if point[0] < player_dc and mode.map[p_c - 1, p_r] == 1:
+                mode.p[0].c -= 1
+            elif point[0] > player_dc and mode.map[p_c + 1, p_r] == 1:
+                mode.p[0].c += 1
+        if point[0] == player_dc:
+            if point[1] < player_dr and mode.map[p_c, p_r - 1] == 1:
+                mode.p[0].r -= 1
+            elif point[1] > player_dr and mode.map[p_c, p_r + 1] == 1:
+                mode.p[0].r += 1
+
+    mode.display.update_pos(mode.p[0].c, mode.p[0].r)
+
+    for c in range(mode.display.columns):
+        for r in range(mode.display.rows):
+            mode.frame[c, r] = (26, 26, 26) if mode.map[c + mode.display.c, r + mode.display.r] == 0 else (229, 229, 229)
+    
+    mode.frame[mode.p[0].c - mode.display.c, mode.p[0].r - mode.display.r] = (255,0,0)
+
+    return mode.frame
+
 def coords2pixels(c,r,buffer):
     x = c*buffer
     y = r*buffer
@@ -44,12 +81,6 @@ def pixels2coords(x,y,buffer):
     c = x//buffer
     r = y//buffer
     return (c,r)
-
-# def random_color():
-#     levels = range(32,256,32)
-#     return tuple(random.choice(levels) for _ in range(3))
-
-game_name = "LED grid"
 
 width = 800
 height = 500
@@ -66,23 +97,31 @@ frames_size = 1
 if frames_size < 1: frames_size = 1
 frames_color = (0,0,0)
 
-player_c = 3
-player_r = 5
-player_color = (128, 0, 0)
-player_move = "idle"
-
-map = pygame.image.load('maze.png')
-map_bitarray = pygame.surfarray.array2d(map)
-level = Level(map_bitarray)
 display = Display(display_cols, display_rows)
+#Menu mode set up
+menu_map = np.zeros((16,10))
+menu = Mode("Menu", menu_map, display, None)
+
+#Maze mode set up
+player = Player((1, 1), (200, 0, 0))
+map_image = pygame.image.load('maze.png')
+maze_map = pygame.surfarray.array2d(map_image)
+maze = Mode("Maze", maze_map, display, maze_logic, players=[player])
+
+#Snake mode set up
+player = Player((8, 5), (255, 255, 0))
+snake_map = np.zeros((16,10))
+snake = Mode("Snake", snake_map, display, None, players=[player])
+
+mode = maze
 
 pygame.init()
-pygame.display.set_caption(game_name)
-# screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.display.set_caption(mode.name)
 screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
             
 while True:
+    # Input
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             pygame.quit()
@@ -93,46 +132,15 @@ while True:
         else: 
             clicked_cell = None
 
-    player_dc = player_c - display.c
-    player_dr = player_r - display.r
-    if clicked_cell:
-        if clicked_cell[1] == player_dr:
-            if clicked_cell[0] < player_dc:
-                player_move = "left"
-            elif clicked_cell[0] > player_dc:
-                player_move = "right"
-        if clicked_cell[0] == player_dc:
-            if clicked_cell[1] < player_dr:
-                player_move = "up"
-            elif clicked_cell[1] > player_dr:
-                player_move = "down"
-    else:
-        player_move = "idle"
+    frame = mode.logic(mode, [clicked_cell])
 
-    keys = pygame.key.get_pressed()
-    if (keys[pygame.K_w] or player_move == "up") and level.map[player_c, player_r - 1] == 1:
-        player_r -= 1
-    if (keys[pygame.K_s] or player_move == "down") and level.map[player_c, player_r + 1] == 1:
-        player_r += 1
-    if (keys[pygame.K_a] or player_move == "left") and level.map[player_c - 1, player_r] == 1:
-        player_c -= 1
-    if (keys[pygame.K_d] or player_move == "right") and level.map[player_c + 1, player_r] == 1:
-        player_c += 1
-    display.update_pos(player_c, player_r)
-
-    for c in range(display.columns):
-        for r in range(display.rows):
-            pos = coords2pixels(c,r, cell_size)
-            x, y = pos[0], pos[1]
+    for c in range(display_cols):
+        for r in range(display_rows):
+            x, y = coords2pixels(c,r, cell_size)
             frame_rect = pygame.Rect(x, y, cell_size, cell_size)
             cell = pygame.Rect(x, y, cell_size, cell_size)
-            cell_color = "gray90" if level.map[c + display.c, r + display.r] == 0 else "gray5"
-            pygame.draw.rect(screen, cell_color, cell)
+            pygame.draw.rect(screen, frame[c, r], cell)
             pygame.draw.rect(screen, frames_color, frame_rect, frames_size)
 
-    player_x, player_y = coords2pixels(player_c - display.c,player_r - display.r, cell_size)
-    player_rect = pygame.Rect(player_x, player_y, cell_size, cell_size)
-    pygame.draw.rect(screen, player_color, player_rect)
-
     pygame.display.update()
-    clock.tick(16)
+    clock.tick(8)
