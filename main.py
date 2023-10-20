dc31d3ccb6a61bd14121f10cf16e6cce31163328
JackPoulis <jackpoulis@gmail.com>
2023-10-20 22:38:51 +0300

    Seperate Classes and methods to new utils.py file

--- main.py
+++ main.py
@@ -1,48 +1,7 @@
 import pygame
 import numpy as np
 from sys import exit
-
-class Display():
-    def __init__(self, columns, rows, background=(0,0,0), position = (0,0), map_size = (32, 32), wall = 3):
-        self.cell = np.full((columns, rows, 3), (0,0,0), dtype=tuple)
-        self.columns = columns
-        self.rows = rows
-        self.background = background
-        self.c = position[0]
-        self.r = position[1]
-        self.wall = wall
-        self.map_size = map_size
-
-    def update_pos(self, player_x, player_y):
-        if (self.c + self.columns - self.wall - 1) < player_x:
-            self.c = player_x - self.columns + self.wall + 1
-            if self.c + self.columns > self.map_size[0]: self.c = self.map_size[0] - self.columns
-        if (self.c + self.wall) > player_x:
-            self.c = player_x - self.wall
-            if self.c < 0: self.c = 0
-        if (self.r + self.rows - self.wall - 1) < player_y:
-            self.r = player_y - self.rows + self.wall + 1 
-            if self.r + self.rows > self.map_size[1]: self.r = self.map_size[1] - self.rows
-        if (self.r + self.wall) > player_y:
-            self.r = player_y - self.wall
-            if self.r < 0: self.r = 0
-
-class Player():
-    def __init__(self, position = (0,0), color = (0,0,255)):
-        self.c = position[0]
-        self.r = position[1]
-        self.color = color
-
-class Mode():
-    def __init__(self, name, map: np.array, display, logic, players = None):
-        self.name = name
-        self.map = map
-        self.cols = map.shape[0]
-        self.rows = map.shape[1]
-        self.p = players
-        self.logic = logic
-        self.display = display
-        self.frame = np.full((display.columns, display.rows, 3), (0,0,0))
+from utils import *
 
 def maze_logic(mode, input):
     point = input[0]
@@ -66,21 +25,11 @@ def maze_logic(mode, input):
 
     for c in range(mode.display.columns):
         for r in range(mode.display.rows):
-            mode.frame[c, r] = (26, 26, 26) if mode.map[c + mode.display.c, r + mode.display.r] == 0 else (229, 229, 229)
+            mode.frame[c, r] = (229, 229, 229) if mode.map[c + mode.display.c, r + mode.display.r] == 0 else (26, 26, 26)
     
     mode.frame[mode.p[0].c - mode.display.c, mode.p[0].r - mode.display.r] = (255,0,0)
 
     return mode.frame
-
-def coords2pixels(c,r,buffer):
-    x = c*buffer
-    y = r*buffer
-    return (x,y)
-
-def pixels2coords(x,y,buffer):
-    c = x//buffer
-    r = y//buffer
-    return (c,r)
 
 width = 800
 height = 500
@@ -132,8 +81,10 @@ while True:
         else: 
             clicked_cell = None
 
+    #New frame
     frame = mode.logic(mode, [clicked_cell])
 
+    #Draw frame
     for c in range(display_cols):
         for r in range(display_rows):
             x, y = coords2pixels(c,r, cell_size)
---
+++ utils.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+class Display():
+    def __init__(self, columns, rows, position = (0,0), map_size = (32, 32), wall = 3):
+        self.columns = columns
+        self.rows = rows
+        self.c = position[0]
+        self.r = position[1]
+        self.wall = wall
+        self.map_size = map_size
+
+    def update_pos(self, player_x, player_y):
+        if (self.c + self.columns - self.wall - 1) < player_x:
+            self.c = player_x - self.columns + self.wall + 1
+            if self.c + self.columns > self.map_size[0]: self.c = self.map_size[0] - self.columns
+        if (self.c + self.wall) > player_x:
+            self.c = player_x - self.wall
+            if self.c < 0: self.c = 0
+        if (self.r + self.rows - self.wall - 1) < player_y:
+            self.r = player_y - self.rows + self.wall + 1 
+            if self.r + self.rows > self.map_size[1]: self.r = self.map_size[1] - self.rows
+        if (self.r + self.wall) > player_y:
+            self.r = player_y - self.wall
+            if self.r < 0: self.r = 0
+
+class Player():
+    def __init__(self, position = (0,0), color = (0,0,255)):
+        self.c = position[0]
+        self.r = position[1]
+        self.color = color
+
+class Mode():
+    def __init__(self, name, map: np.array, display, logic, players = None):
+        self.name = name
+        self.map = map
+        self.cols = map.shape[0]
+        self.rows = map.shape[1]
+        self.p = players
+        self.logic = logic
+        self.display = display
+        self.frame = np.full((display.columns, display.rows, 3), (0,0,0))
+
+def coords2pixels(c,r,buffer):
+    x = c*buffer
+    y = r*buffer
+    return (x,y)
+
+def pixels2coords(x,y,buffer):
+    c = x//buffer
+    r = y//buffer
+    return (c,r)
