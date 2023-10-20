215cafea6f6161762ade6dce2506f30a1991c940
JackPoulis <jackpoulis@gmail.com>
2023-10-20 11:38:55 +0300

    Remove redundant variables from display class

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ import numpy as np
 import random
 from sys import exit
 
-class Level():
-    def __init__(self, map: np.array):
+class Game():
+    def __init__(self, name, map: np.array):
+        self.name = name
         self.map = map
         self.x = map.shape[0]
         self.y = map.shape[1]
 
 class Display():
-    def __init__(self, columns, rows, background=(0,0,0), frames_color = (255,255,255), position = (0,0), cell_size = 50, frames_size = 5, map_size = (32, 32), wall = 3):
+    def __init__(self, columns, rows, background=(0,0,0), position = (0,0), map_size = (32, 32), wall = 3):
         self.cell = np.full((columns, rows, 3), (0,0,0), dtype=tuple)
         self.columns = columns
         self.rows = rows
         self.background = background
-        self.frames_color = frames_color
         self.c = position[0]
         self.r = position[1]
-        self.cell_size = cell_size
-        self.frames_size = frames_size
         self.wall = wall
         self.map_size = map_size
 
@@ -47,9 +45,9 @@ def pixels2coords(x,y,buffer):
     r = y//buffer
     return (c,r)
 
-def random_color():
-    levels = range(32,256,32)
-    return tuple(random.choice(levels) for _ in range(3))
+# def random_color():
+#     levels = range(32,256,32)
+#     return tuple(random.choice(levels) for _ in range(3))
 
 game_name = "LED grid"
 
@@ -66,16 +64,17 @@ cell_size = cell_width if cell_width < cell_height else cell_height
 # frames_size = cell_size//50
 frames_size = 1 
 if frames_size < 1: frames_size = 1
+frames_color = (0,0,0)
 
-player_c = 4
+player_c = 3
 player_r = 5
-player_color = (255, 0, 0)
+player_color = (128, 0, 0)
 player_move = "idle"
 
 map = pygame.image.load('maze.png')
 map_bitarray = pygame.surfarray.array2d(map)
 level = Level(map_bitarray)
-display = Display(display_cols, display_rows, cell_size = cell_size, frames_size = frames_size,  frames_color = (0,0,0))
+display = Display(display_cols, display_rows)
 
 pygame.init()
 pygame.display.set_caption(game_name)
@@ -90,7 +89,7 @@ while True:
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mouse_pos = pygame.mouse.get_pos()
-            clicked_cell = pixels2coords(mouse_pos[0], mouse_pos[1], display.cell_size)
+            clicked_cell = pixels2coords(mouse_pos[0], mouse_pos[1], cell_size)
         else: 
             clicked_cell = None
 
@@ -123,16 +122,16 @@ while True:
 
     for c in range(display.columns):
         for r in range(display.rows):
-            pos = coords2pixels(c,r,display.cell_size)
+            pos = coords2pixels(c,r, cell_size)
             x, y = pos[0], pos[1]
-            frame_rect = pygame.Rect(x, y, display.cell_size, display.cell_size)
-            cell = pygame.Rect(x, y, display.cell_size, display.cell_size)
+            frame_rect = pygame.Rect(x, y, cell_size, cell_size)
+            cell = pygame.Rect(x, y, cell_size, cell_size)
             cell_color = "gray90" if level.map[c + display.c, r + display.r] == 0 else "gray5"
             pygame.draw.rect(screen, cell_color, cell)
-            pygame.draw.rect(screen, display.frames_color, frame_rect, display.frames_size)
+            pygame.draw.rect(screen, frames_color, frame_rect, frames_size)
 
-    player_x, player_y = coords2pixels(player_c - display.c,player_r - display.r,display.cell_size)
-    player_rect = pygame.Rect(player_x, player_y, display.cell_size, display.cell_size)
+    player_x, player_y = coords2pixels(player_c - display.c,player_r - display.r, cell_size)
+    player_rect = pygame.Rect(player_x, player_y, cell_size, cell_size)
     pygame.draw.rect(screen, player_color, player_rect)
 
     pygame.display.update()
