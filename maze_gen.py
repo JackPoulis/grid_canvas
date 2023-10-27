16205e1776d3df227d7f368de516c63bc2564fdd
JackPoulis <jackpoulis@gmail.com>
2023-10-27 17:13:00 +0300

    Remove bug on algorithm while loop

--- maze_gen.py
+++ maze_gen.py
@@ -7,9 +7,9 @@ maze = np.full((width, height), 1)
 
 def visit(x, y):
     maze[x, y] = 0
-    unvisited_neighbors = []
 
     while True:
+        unvisited_neighbors = []
         if y > 1 and (x, y - 2) not in visited:
             unvisited_neighbors.append((x, y - 2))
 
@@ -22,19 +22,14 @@ def visit(x, y):
         if x < width - 2 and (x + 2, y) not in visited:
             unvisited_neighbors.append((x + 2, y))
 
-        print(unvisited_neighbors)
         if len(unvisited_neighbors) == 0:
-            
             return
         else:
             next = random.choice(unvisited_neighbors)
             maze[(next[0]+x)//2,(next[1]+y)//2] = 0
-        
+
             visited.append(next)
-            print(visited)
             visit(next[0],next[1])
             
-
 visited = [(1,1)]
 visit(1,1)
-print(maze)
