ef119b009e94d149b274841291452d79f521893d
JackPoulis <jackpoulis@gmail.com>
2024-03-24 12:28:08 +0200

    Create object class

--- maze.py
+++ maze.py
@@ -5,13 +5,12 @@ import random
 
 class Maze(Mode):
 
-    def __init__(self, name, map, players, thumbnail):
-        super().__init__(name, map, players =  players, thumbnail = thumbnail)
+    def __init__(self, name, map, players, objects, thumbnail):
+        super().__init__(name, map, players = players, objects = objects, thumbnail = thumbnail)
 
     def logic(self, input):
         p_c = self.p[0].c
         p_r = self.p[0].r
-        self.p[0].r
         p_dc = p_c - self.d_c
         p_dr = p_r - self.d_r
         if input[0]:
@@ -39,9 +38,12 @@ class Maze(Mode):
     def next_frame(self):
         to_rgb = lambda x: np.array([229, 229, 229]) if x == 0 else np.array([26, 26, 26])
         frame_bits = self.map[self.d_c:self.d_c+self.d_cols, self.d_r:self.d_r+self.d_rows]
-        frame = np.array([np.fromiter(map(to_rgb, x), dtype=np.ndarray) for x in frame_bits])        
-        frame[self.p[0].c - self.d_c, self.p[0].r - self.d_r] = self.p[0].color
-
+        frame = np.array([np.fromiter(map(to_rgb, x), dtype=np.ndarray) for x in frame_bits])
+        end = self.objects["end"]
+        player = self.p[0]
+        frame[end.x - self.d_c, end.y - self.d_r] = end.color      
+        frame[player.c - self.d_c, player.r - self.d_r] = player.color
+        
         return frame
 
 def generate_maze(width,height):
@@ -79,10 +81,11 @@ def generate_maze(width,height):
 
 #Maze mode set up
 maze_player = Player((1, 1), (255, 0, 0))
+maze_end = Object("end", (3, 3), (0, 255, 0))
 maze_thumbnail_image = pygame.image.load('thumbnails/maze.png')
 maze_thumbnail = pygame.surfarray.array3d(maze_thumbnail_image)
 maze_map = generate_maze(31,31)
-maze = Maze("Maze", maze_map, players=[maze_player], thumbnail=maze_thumbnail)
+maze = Maze("Maze", maze_map, players=[maze_player], objects = [maze_end], thumbnail=maze_thumbnail)
 
 if __name__ == "__main__":
     width = 51   
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Player():
     def __init__(self, position = (0,0), color = (0,0,255)):
@@ -5,30 +6,43 @@ class Player():
         self.r = position[1]
         self.color = color
 
+class Object():
+    obj_id = 0
+    def __init__(self, name, position, color = (128,0,128)) -> None:
+        self.name = name
+        self.id = Object.obj_id
+        Object.obj_id += 1
+        self.x, self.y = position
+        self.color = color
+
 class Mode():
-    def __init__(self, name, map, players = None, thumbnail = None):
+    def __init__(self, name, map, players = None, objects = None, thumbnail = None):
         self.name = name
         self.map = map
         self.cols = map.shape[0]
         self.rows = map.shape[1]
         self.p = players
+        self.objects = {}
+        if objects:
+            for obj in objects:
+                self.objects[obj.name] = obj
         self.d_cols = 16
         self.d_rows = 10
         self.d_c = 0
         self.d_r = 0
         self.thumbnail = thumbnail
-        self.mode = 0
 
     #Overwrite
-    def logic(input):
-        if input[0]:
-            x, y = input[0]
-            output = 0
-        return output
+    def logic(self, input):
+        #template
+        # if input[0]:
+        #     x, y = input[0]
+        return 0
     
     #Overwrite
-    def next_frame():
-        pass
+    def next_frame(self) -> np.array:
+        frame = np.full((self.cols, self.rows), (0,0,0))
+        return frame
     
 def coords2pixels(c,r,buffer):
     x = c*buffer
